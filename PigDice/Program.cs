2961442fbd6b9cf3b9d04f5186a96aa37d847821
max-student <max-student@maxtrain.com>
2021-02-19 15:02:31 -0500

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PigDice {
+    class Program {
+        //adding methods
+        static int RollDie() {
+            var rnd = new Random();
+            return rnd.Next(1, 7);
+
+        }
+        static int PlayPigDice() {
+            var score = 0;
+            var roll = RollDie();
+            while (roll != 1) {
+                score += roll;
+                roll = RollDie();
+            }
+            return score;
+        }
+        //main method
+        static void Main(string[] args) {
+            var highScore = 250;
+            long counter = 0;
+            var score = PlayPigDice();
+            while (score < highScore) {
+                if (score <= highScore) {
+                    score = PlayPigDice();
+                    counter++;
+
+                }
+
+
+
+            }
+
+
+            Console.WriteLine($"High score is{score} in {counter} games");
+        }
+    }
+}
+
